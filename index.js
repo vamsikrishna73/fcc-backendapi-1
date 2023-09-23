7266788c28c907f037f04565abca3ff5dcbbd422
vamsikrishna73 <vamsikrishnarunkana73@gmail.com>
2023-09-23 11:32:49 +0530

    second_commit

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.get("/api/hello", function (req, res) {
 
 
 // listen for requests :)
-var listener = app.listen(3000, function () {
+var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
 
